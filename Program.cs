579a0e243b89608999458fd2ed345df5060b9a4e
IasmimHolanda <iasmim_holanda@hotmail.com>
2024-01-08 12:21:46 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Smartphone nokiaPhone = new Nokia("123456789", "Nokia 8", "IMEI123", 64);
+            nokiaPhone.Ligar();
+            nokiaPhone.ReceberLigacao();
+            nokiaPhone.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            Smartphone iPhone = new Iphone("987654321", "iPhone 12", "IMEI456", 128);
+            iPhone.Ligar();
+            iPhone.ReceberLigacao();
+            iPhone.InstalarAplicativo("Instagram");
+        }
+    }
+}
